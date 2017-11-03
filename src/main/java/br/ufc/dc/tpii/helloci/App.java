4dc13e5f75affcac88b4235157885eaee94474d4
Lincoln Rocha <lincoln@dc.ufc.br>
2017-11-03 08:52:52 -0300

    adding maven nature

---
+++ src/main/java/br/ufc/dc/tpii/helloci/App.java
@@ -0,0 +1,11 @@
+package br.ufc.dc.tpii.helloci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public String sayHello() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/br/ufc/dc/tpii/helloci/AppTest.java
@@ -0,0 +1,15 @@
+package br.ufc.dc.tpii.helloci;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testSayHello() {
+		App app = new App();
+		assertEquals("Hello World", app.sayHello());
+	}
+
+}
